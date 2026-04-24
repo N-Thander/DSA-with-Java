f33ca0ded4ff762c092fec455e425f9484c2269d
N-Thander <nasiruddinthander2002@gmail.com>
2026-04-24 16:25:54 +0530

    main: fix local terminal commands for future development

--- src/datastrucutres/queue/Main.java
+++ src/datastrucutres/queue/Main.java
@@ -2,18 +2,17 @@ package datastrucutres.queue;
 
 public class Main {
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         Queue myQueue = new Queue(10);
 
-//        myQueue.getFirst();
-//        myQueue.getLast();
-//        myQueue.getLength();
+        //        myQueue.getFirst();
+        //        myQueue.getLast();
+        //        myQueue.getLength();
 
         myQueue.enqueue(20);
         myQueue.enqueue(30);
-//        myQueue.enqueue(40);
-//        myQueue.enqueue(50);
+        //        myQueue.enqueue(40);
+        //        myQueue.enqueue(50);
 
         myQueue.printQueue();
         System.out.println();
